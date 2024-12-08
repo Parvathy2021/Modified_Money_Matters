27b53b6df2c9f517006622784308b56141c28130
Tri <superglue2294@gmail.com>
2024-12-07 19:17:56 -0500

    Created directories and created jsx and css files for about, login, and register

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/home/Home'
+import About from './pages/about/About'
+import Login from './pages/login/Login'
+import Register from './pages/register/Register'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,7 +15,10 @@ function App() {
     <>
       <Router>
         <Routes>
-        <Route path='/' element={<Home />}/>
+          <Route path='/' element={<Home />}/>
+          <Route path='/about' element={<About />}/>
+          <Route path='/login' element={<Login />}/>
+          <Route path='/register' element={<Register />}/>
         </Routes>
       </Router>
     </>
