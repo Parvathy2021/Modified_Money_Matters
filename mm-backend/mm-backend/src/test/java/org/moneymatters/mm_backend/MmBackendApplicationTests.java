00e4c569cb5b91a722e8ea93195a1948bdd4b491
Cypress <kad2321@yahoo.com>
2024-12-19 16:38:50 -0500

    Attempt 2 at fixing backend

---
+++ mm-backend/mm-backend/src/test/java/org/moneymatters/mm_backend/MmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.moneymatters.mm_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
