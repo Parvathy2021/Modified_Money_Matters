f9fa769d915ae5acd47b1babfef9ffdf078b0952
timwebdev1 <tlmcdermott79@gmail.com>
2024-12-29 21:05:17 -0600

    created Tag Class, updated dependencies

--- src/main/java/org/moneymatters/mm_backend/models/Tag.java
+++ src/main/java/org/moneymatters/mm_backend/models/Tag.java
@@ -1,8 +1,17 @@
 package org.moneymatters.mm_backend.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Tag {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     private String name;
     private String color;
     private boolean isDefault;
