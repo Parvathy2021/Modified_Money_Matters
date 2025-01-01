789859fb97467c3538b2ca50a2d5c7fb2d9eb50c
Kelly Doherty <kad2321@yahoo.com>
2025-01-01 17:17:26 -0500

    Created abstract class and Transaction class

---
+++ src/main/java/org/moneymatters/mm_backend/models/Entry.java
@@ -0,0 +1,90 @@
+package org.moneymatters.mm_backend.models;
+
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.util.Objects;
+
+@MappedSuperclass
+public abstract class Entry {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotNull
+    @NotBlank(message = "Amount cannot be blank")
+    private int amount;
+
+    private boolean isIncome = false;
+
+    @Size(max = 200, message = "Description cannot be larger than 200 characters")
+    private String description;
+
+    private int budgetId;
+
+    private int userId;
+
+    public int getId() {
+        return id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getBudgetId() {
+        return budgetId;
+    }
+
+    public void setBudgetId(int budgetId) {
+        this.budgetId = budgetId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isIncome() {
+        return isIncome;
+    }
+
+    public void setIncome(boolean income) {
+        isIncome = income;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Entry that = (Entry) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
---
+++ src/main/java/org/moneymatters/mm_backend/models/RecurringTransaction.java
@@ -0,0 +1,7 @@
+package org.moneymatters.mm_backend.models;
+
+public class RecurringTransaction {
+
+
+
+}
