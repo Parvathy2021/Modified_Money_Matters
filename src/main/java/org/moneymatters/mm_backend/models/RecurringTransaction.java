b4e1e252b04716d59f9041708f26a76e73b002e3
Kelly Doherty <kad2321@yahoo.com>
2025-01-01 18:58:47 -0500

    Building out the RecurringTransaction class

--- src/main/java/org/moneymatters/mm_backend/models/RecurringTransaction.java
+++ src/main/java/org/moneymatters/mm_backend/models/RecurringTransaction.java
@@ -1,7 +1,47 @@
 package org.moneymatters.mm_backend.models;
 
-public class RecurringTransaction {
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+
+@Entity
+public class RecurringTransaction extends Entry {
 
 
+    @NotNull (message = "Day cannot be null")
+    @NotBlank(message = "Day cannot be blank")
+    @Min(value = 1, message = "Number cannot be less than 1 or greater than 31")
+    @Max(value = 31, message = "Number cannot be less than 1 or greater than 31")
+    private int recurringDay;
+
+    private LocalDate nextTransactionDate;
+
+    //Need to create a method to calculate the nextTransactionDate -Cy
+
+
+    public RecurringTransaction(int recurringDay) {
+        super();
+        this.recurringDay = recurringDay;
+    }
+
+    public int getRecurringDay() {
+        return recurringDay;
+    }
+
+    public void setRecurringDay(int recurringDay) {
+        this.recurringDay = recurringDay;
+    }
+
+    public LocalDate getNextTransactionDate() {
+        return nextTransactionDate;
+    }
+
+    public void setNextTransactionDate(LocalDate nextTransactionDate) {
+        this.nextTransactionDate = nextTransactionDate;
+    }
 
 }
