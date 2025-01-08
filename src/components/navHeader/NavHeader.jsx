ea3392c356ece4bb8ac4cc2835ca28e0aa10856a
Tri <superglue2294@gmail.com>
2025-01-07 19:52:03 -0500

    created landing page

--- src/components/navHeader/NavHeader.jsx
+++ src/components/navHeader/NavHeader.jsx
@@ -5,12 +5,12 @@ import './NavHeader.css'
 
 const NavHeader = () => {
     return (
-        <div>
+        <div className='nav-link'>
             <nav>
-                <Link className='nav-link' to='/'>Home</Link>
-                <Link className='nav-link' to='/about'>About</Link>
-                <Link className='nav-link' to='/login'>Login</Link>
-                <Link className='nav-link' to='/register'>Register</Link>
+                <Link className='link-pad' to='/'>Home</Link>
+                <Link className='link-pad' to='/about'>About</Link>
+                <Link className='link-pad' to='/login'>Login</Link>
+                <Link className='link-pad' to='/register'>Register</Link>
             </nav>
         </div>
     )
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,66 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+import './Home.css'
+
+import moneyImage from '../../assets/money-gold-coin-growth-concept-illustration-stacks-of-gold-coin-like-income-graph-with-dollar-free-vector.jpg'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <article className="h-screen overflow-y-scroll snap-y snap-mandatory">
+        <section className="min-h-full snap-start flex items-center justify-center">
+          <div className="flex-1 text-center">
+            <article>
+              <img src={moneyImage} alt="Money" className="mx-auto" />
+            </article>
+          </div>
+          <div className="flex-1 text-center">
+            <article>
+              <h1>Manage Your Budget</h1>
+              <br />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora sapiente autem, fugiat quidem cupiditate maiores ullam praesentium nisi architecto harum, velit corporis quibusdam aut veritatis saepe inventore amet, quia iusto! Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis eum pariatur aut nostrum ex, eligendi magnam provident, reprehenderit dolores impedit voluptas earum odio, magni amet deserunt corporis. Nihil, modi facere.</p>
+            </article>
+          </div>
+        </section>
+        <section className="min-h-full snap-start flex items-center justify-center">
+          <div className="flex-1 text-center">
+            <article>
+              <h1>Track Your Expenses</h1>
+              <br />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora sapiente autem, fugiat quidem cupiditate maiores ullam praesentium nisi architecto harum, velit corporis quibusdam aut veritatis saepe inventore amet, quia iusto! Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis eum pariatur aut nostrum ex, eligendi magnam provident, reprehenderit dolores impedit voluptas earum odio, magni amet deserunt corporis. Nihil, modi facere.</p>
+            </article>
+          </div>
+          <div className="flex-1 text-center">
+            <article>
+              <img src={moneyImage} alt="Money" className="mx-auto" />
+            </article>
+          </div>
+        </section>
+        <section className="min-h-full snap-start flex items-center justify-center">
+          <div className="flex-1 text-center">
+            <article>
+              <img src={moneyImage} alt="Money" className="mx-auto" />
+            </article>
+          </div>
+          <div className="flex-1 text-center">
+            <article>
+              <h1>Get Started</h1>
+              <br />
+              <Link to="/login">
+                <button className="rounded-full px-4 py-2 bg-blue-500 text-white">Login</button>
+              </Link>
+              <br />
+              <br />
+              <Link to="/register">
+                <button className="rounded-full px-4 py-2 bg-blue-500 text-white">Register</button>
+              </Link>
+            </article>
+          </div>
+        </section>
+      </article>
+
+    </div>
   )
 }
 
