a714873ea93f860992d17103d696db1b49852cb3
timwebdev1 <tlmcdermott79@gmail.com>
2025-01-08 18:46:15 -0600

    updated .idea.workspace.xlm error by adding closing tag causing load error

--- src/main/java/org/moneymatters/mm_backend/controllers/UserController.java
+++ src/main/java/org/moneymatters/mm_backend/controllers/UserController.java
@@ -2,9 +2,12 @@ package org.moneymatters.mm_backend.controllers;
 
 import org.moneymatters.mm_backend.data.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
 
     @Autowired
