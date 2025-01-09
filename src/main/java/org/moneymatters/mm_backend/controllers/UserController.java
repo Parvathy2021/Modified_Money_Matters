06b40bd69a608a4437037ef7d798a64baf8f955d
timwebdev1 <tlmcdermott79@gmail.com>
2025-01-08 19:47:45 -0600

    Updated User Controller, added placeholders for CRUD

--- src/main/java/org/moneymatters/mm_backend/controllers/UserController.java
+++ src/main/java/org/moneymatters/mm_backend/controllers/UserController.java
@@ -2,8 +2,8 @@ package org.moneymatters.mm_backend.controllers;
 
 import org.moneymatters.mm_backend.data.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -13,6 +13,27 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+//    Placeholder for creating new user
+    @PostMapping
+    public String createUser() {
+        return "Create user endpoint";
+    }
 
+//    Placeholder for retrieving user by id
+    @GetMapping("/{id}")
+    public String getUserById(@PathVariable int id)  {
+        return "Retrive user by id endpoint";
+    }
 
+//    Placeholder for updating user
+    @PutMapping("/{id}")
+    public String updateUser(@PathVariable int id) {
+        return "Update user endpoint";
+    }
+
+// Placehold for deleting a user
+    @DeleteMapping("/{id}")
+    public String deleteUser(@PathVariable int id) {
+        return "Delete user";
+    }
 }
