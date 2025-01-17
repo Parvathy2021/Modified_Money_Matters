1da2ca354ac492b2521cbd3a4d321ca28f10238c
Cypress <kad2321@yahoo.com>
2025-01-17 13:21:14 -0500

    Added a sql file to seed some users into the database.

---
+++ src/main/resources/userData.sql
@@ -0,0 +1,8 @@
+INSERT INTO user (confirm_password, email, pwhash, username)
+VALUES ('T3sting!', 'kad2321@yahoo.com', '$2a$12$sTaxuj4V1kHhNCX/7EB76edBMrxvsMXNSX9vWZc3AzXKzjp6E/0Be', 'CyGuy')
+
+INSERT INTO user (confirm_password, email, pwhash, username)
+VALUES ('Group3@', 'test@test.com', '$2a$12$rA77x//yuZyH9Ajmykhof./HGdnhkmVuz9.r9pD/KY01F7nk8AFqq', 'admin' )
+
+INSERT INTO user (confirm_password, email, pwhash, username)
+VALUES ('Peanut2025#', 'anothertest@test.com', '$2a$12$dKuuI3CcLDsge020MmipFuzaX/DYFZtp0xS9d29lEryQUwZ5NzX3O', 'NewUser')
