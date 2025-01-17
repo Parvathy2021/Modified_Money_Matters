d2d11750d6af8a23c2d5c543a297b7dd3c5e3035
Cypress <kad2321@yahoo.com>
2025-01-17 10:25:49 -0500

    Expense UI showing up, recurring date toggles based on checkbox. Added custom colors to tailwind.config.js

--- src/Components/Expense/Expense.jsx
+++ src/Components/Expense/Expense.jsx
@@ -1,5 +1,4 @@
 import React, {useState} from 'react';
-import RecurringDate from './RecurringDate';
 import {Link} from 'react-router-dom';
 
 function Expense() {
@@ -7,32 +6,53 @@ function Expense() {
     const [description, setDescription] = useState('');
     const [isRecurring, setIsRecurring] = useState(false);
     const [isIncome, setIsIncome] = useState(false);
+    const [recurringDate, setRecurring] = useState('');
 
     return (
         <>
-        <h1>New Transaction</h1>
-        <form>
-            <label>Amount 
-                <input type="text" value={amount} onChange={(e) => setAmount(e.target.value)}></input>
-            </label>
-            <label>Select if transaction is income: 
-                <input type="checkbox" value={isIncome} onChange={(e) => setIsIncome(e.value.isIncome)}></input>
-            </label>
-            <label>Select if transaction is recurring: 
-                <input type="checkbox" value={isRecurring} onChange={(e) => setIsRecurring(e.value.isRecurring)}></input>
-            </label>
-            {/*Want to make the date input show only if isRecurring is checked. */}
-            {/* <label style= {!isRecurring ? 'display' : 'display:none'}>Recurring Day's Date (1-31) 
-                {<RecurringDate />}
-            </label> */}
+        
+        <div className='flex justify-center items-center min-h-screen bg-black'>
+            <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
+        
+                <h1 className='text-2xl font-semibold text-center mb-6 text-black'>New Transaction</h1>
+      
+                    <form>
+                        <div className='block text-black'>
+           
+                            <label    className='mt-2 p-2 w-full border border-white rounded-md focus:outline-none focus:ring-2 focus:ring-blue block'>Amount
+                                <input type="text" value={amount} onChange={(e) => setAmount(e.target.value)}className='mt-2 p-2 w-full border border-gray-300 bg-white rounded-md focus:outline-none focus:ring-2 focus:ring-blue' ></input>
+                             </label>
+           
+                             <label    className='mt-2 p-2 w-full border border-white rounded-md focus:outline-none focus:ring-2 focus:ring-blue block' >Select if transaction is income: 
+                                <input type="checkbox" checked={isIncome} onChange={(e) => setIsIncome(e.target.checked)}className='mt-2 p-2 border border-black rounded-md focus:outline-none focus:ring-2 focus:ring-blue block' ></input>
+                            </label>
+            
+                            <label    className='mt-2 p-2 w-full border border-white rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 block' >Select if transaction is recurring: 
+                                <input type="checkbox" checked={isRecurring} onChange={(e) => setIsRecurring(e.target.checked)}className='mt-2 p-2 border border-black rounded-md focus:outline-none focus:ring-2 focus:ring-blue block' ></input>
+                            </label>
+          
+                            {isRecurring &&  (
+                                <label    className='mt-2 p-2 border border-white rounded-md focus:outline-none focus:ring-2 focus:ring-blue block' > Recurring Day's Date (1-31) 
+                                    <input type="text" value={recurringDate} onChange={(e) => setRecurring(e.target.value)}className='mt-2 p-2 border border-black rounded-md focus:outline-none focus:ring-2 focus:ring-blue bg-white' ></input>
+                                </label>    
+                            )}
+            
 
-            <label>Description 
-                <input type="text" value={description} onChange={(e) => setDescription(e.target.description)}></input>
-            </label>
+                            <label    className='mt-2 p-2 w-full border border-white rounded-md focus:outline-none focus:ring-2 focus:ring-blue block' >Description 
+                                <input type="text" value={description} onChange={(e) => setDescription(e.target.value)}className='mt-2 p-2 border border-gray-300 bg-white rounded-md focus:outline-none focus:ring-2 focus:ring-blue block' ></input>
+                            </label>
 
-            <button>Submit</button>
-        </form>
+                            <button  className='w-full py-2 bg-lightblue text-white rounded-md hover:bg-green 
+                            hover:text-black focus:outline-none focus:ring-2 focus:ring-blue'>
+                                Submit
+                            </button>
+           
+                        </div>
+                    </form>
+                </div>
+            </div>
         </>
+      
     )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+      lightgreen: '#bbff9e',
+      lightred: '#ff7f7f',
+      lightblue: '#a7c7e7',
+      black: '#141414',
+      offwhite: '#eef0f2'
+    }
+  },
+    
   },
   plugins: [],
 }
