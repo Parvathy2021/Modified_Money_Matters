a51582c152d64d38cb58afd8b0b2196badf5e8ed
Parvathy2021 <parvathyvenu14@gmail.com>
2025-01-21 21:48:04 -0600

    Added Date, Recurring frequency, added Split transaction.

--- src/Components/Expense/Expense.jsx
+++ src/Components/Expense/Expense.jsx
@@ -1,17 +1,20 @@
+import { split } from 'postcss/lib/list';
 import React, {useState} from 'react';
 
 function Expense() {
     const [amount, setAmount] = useState('');
+    const [date, setDate] = useState('');
     const [description, setDescription] = useState('');
     const [isRecurring, setIsRecurring] = useState(false);
     const [isIncome, setIsIncome] = useState(false);
     const [recurringDate, setRecurring] = useState('');
     const [frequency, setFrequency] = useState('monthly')
     const [tag, setTag] = useState('')
-
+    const [isSplits, setIsSplits] = useState([{tag: "", amount: 0}]);
+ 
     const handleSubmit = (e) => {
         e.preventDefault();
-        const transaction = [amount, isIncome, isRecurring, recurringDate, frequency, description, tag];
+        const transaction = [amount,date, isSplits, isIncome, isRecurring, recurringDate, frequency, description, tag];
         console.log(transaction)
         fetch("http://localhost:8080/expense", {
             method:"POST",
@@ -20,6 +23,7 @@ function Expense() {
         }).then(()=> {
             console.log("New transaction added.")
         })
+        
     }
 
     return (
@@ -36,12 +40,37 @@ function Expense() {
                             <label    className='mt-2 p-2 w-full border border-white rounded-md focus:outline-none focus:ring-2 focus:ring-blue block'>Amount
                                 <input type="text" value={amount} onChange={(e) => setAmount(e.target.value)}className='mt-2 p-2 w-full border border-gray-300 bg-white rounded-md focus:outline-none focus:ring-2 focus:ring-blue' ></input>
                              </label>
+                             <label htmlFor='date' className='mt-2 p-2 w-full border border-white rounded-md focus:outline-none focus:ring-2 focus:ring-blue block'>Date
+                                <input type="date" id='date' value={date} onChange={(e) => setDate(e.target.value)}className='mt-2 p-2 w-full border border-gray-300 bg-white rounded-md focus:outline-none focus:ring-2 focus:ring-blue' ></input>
+                             </label>
            
                              <label    className='mt-2 p-2 w-full border border-white rounded-md focus:outline-none focus:ring-2 focus:ring-blue block' >Select if transaction is income: 
                                 <input type="checkbox" checked={isIncome} onChange={(e) => setIsIncome(e.target.checked)}className='mt-2 p-2 border border-black rounded-md focus:outline-none focus:ring-2 focus:ring-blue block' ></input>
                             </label>
-            
-                            <label    className='mt-2 p-2 w-full border border-white rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 block' >Select if transaction is recurring: 
+                            <label    className='mt-2 p-2 w-full border border-white rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 block' >Select if transaction need Split Tags: 
+                                <input type="checkbox" checked={isSplits} onChange={(e) => setIsSplits(e.target.checked)}className='mt-2 p-2 border border-black rounded-md focus:outline-none focus:ring-2 focus:ring-blue block' ></input>
+                                
+      
+                            </label>
+                                {isSplits && (
+                                    <label    className='mt-2 p-2 w-full border border-white rounded-md focus:outline-none focus:ring-2 focus:ring-blue block'>Amount
+                                        <input type="text" value={amount} onChange={(e) => setAmount(e.target.value)}className='mt-2 p-2 w-full border border-gray-300 bg-white rounded-md focus:outline-none focus:ring-2 focus:ring-blue' ></input>
+                                        <label className='mt-2 p-2 w-full border border-white rounded-md focus:outline-none focus:ring-2 focus:ring-blue block' >Tag
+                                            <select className='mt-2 p-2 border border-gray-300 bg-white rounded-md focus:outline-none focus:ring-2 focus:ring-blue block'  name="selectedTag" value={tag} onChange={(e) => setTag(e.target.value)}>
+                                            {/* Placeholder options */}
+                                            <option value="null">Please select a tag</option>
+                                            <option value="Rent/Mortgage">Rent/Mortgage</option>
+                                            <option value="Food">Food</option>
+                                            <option value="Clothing">Clothing</option>
+                                            <option value="Misc.">Misc.</option>
+                                            </select>
+                                        </label>
+                                        <button  className='w-full py-2 bg-lightblue text-white rounded-md hover:bg-green 
+                                                 hover:text-black focus:outline-none focus:ring-2 focus:ring-blue' type="button" > Add Split </button>
+                                    </label>
+                                )}   
+
+                            <label className='mt-2 p-2 w-full border border-white rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 block' >Select if transaction is recurring: 
                                 <input type="checkbox" checked={isRecurring} onChange={(e) => setIsRecurring(e.target.checked)}className='mt-2 p-2 border border-black rounded-md focus:outline-none focus:ring-2 focus:ring-blue block' ></input>
                             </label>
           
@@ -58,8 +87,7 @@ function Expense() {
                                     <input type="text" value={recurringDate} onChange={(e) => setRecurring(e.target.value)}className='mt-2 p-2 border border-black rounded-md focus:outline-none focus:ring-2 focus:ring-blue bg-white' ></input>
                                 </label>
                            </label>                                
-                            )}
-                            
+                            )}                           
 
                             <label    className='mt-2 p-2 w-full border border-white rounded-md focus:outline-none focus:ring-2 focus:ring-blue block' >Description 
                                 <input type="text" value={description} onChange={(e) => setDescription(e.target.value)}className='mt-2 p-2 border border-gray-300 bg-white rounded-md focus:outline-none focus:ring-2 focus:ring-blue block' ></input>
