8b26221af5a56e77abb594ece21bdcc9e8e02104
Kelly Doherty <kad2321@yahoo.com>
2025-01-29 00:07:01 -0500

    Trying to get transactions to save goshdarn

---
+++ src/main/java/org/moneymatters/mm_backend/models/dto/TransactionDTO.java
@@ -0,0 +1,79 @@
+package org.moneymatters.mm_backend.models.dto;
+
+
+public class TransactionDTO {
+
+    private Integer userId;
+    private Integer budgetId;
+    private Integer tagId;
+    private int amount;
+    private String description;
+    private boolean isRecurring;
+    private boolean isIncome;
+    private String recurringDate;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getBudgetId() {
+        return budgetId;
+    }
+
+    public void setBudgetId(Integer budgetId) {
+        this.budgetId = budgetId;
+    }
+
+    public Integer getTagId() {
+        return tagId;
+    }
+
+    public void setTagId(Integer tagId) {
+        this.tagId = tagId;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isRecurring() {
+        return isRecurring;
+    }
+
+    public void setRecurring(boolean recurring) {
+        isRecurring = recurring;
+    }
+
+    public boolean isIncome() {
+        return isIncome;
+    }
+
+    public void setIncome(boolean income) {
+        isIncome = income;
+    }
+
+    public String getRecurringDate() {
+        return recurringDate;
+    }
+
+    public void setRecurringDate(String recurringDate) {
+        this.recurringDate = recurringDate;
+    }
+}
+
