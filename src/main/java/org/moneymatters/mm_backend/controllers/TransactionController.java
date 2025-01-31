a620af93f3fb1e8a5ab5db726487463645e40400
Cypress <kad2321@yahoo.com>
2025-01-30 23:45:58 -0500

    Working on getting the data needed from the backend

--- src/main/java/org/moneymatters/mm_backend/controllers/TransactionController.java
+++ src/main/java/org/moneymatters/mm_backend/controllers/TransactionController.java
@@ -169,4 +169,11 @@ public class TransactionController {
     transactionRepository.deleteById(id);
     return new ResponseEntity<>("Transaction deleted successfully", HttpStatus.OK);
 }
+    //Search
+    @GetMapping("/search")
+    public ResponseEntity<?> searchTransactions(@RequestParam String query){
+        List<Transaction> transactions = transactionRepository.findByDescriptionContainingIgnoreCase(query);
+        return new ResponseEntity<>(transactions, HttpStatus.OK);
+    }
+
 }
