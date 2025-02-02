08b101055c3635f6b49a0dea667b4c4097008ad0
Cypress <kad2321@yahoo.com>
2025-02-01 20:15:55 -0500

    Allow user to cancel search with button

--- src/Components/TransactionSearch/TransactionSearch.jsx
+++ src/Components/TransactionSearch/TransactionSearch.jsx
@@ -2,19 +2,24 @@
 import SearchBar from "./SearchBar";
 import { useParams } from "react-router-dom";
 import TransactionList from "./TransactionList";
+import { useNavigate } from "react-router-dom";
 
 
 function TransactionSearch() {
     const { budget_id } = useParams();
-    console.log("Selected Budget ID:", budget_id);
-
+    const navigate = useNavigate();
 
 
     return(
-        <div>
-            <h1>Transaction Search</h1>
-            <TransactionList budget_id={budget_id} />
-        </div>
+       <>
+            <div>
+                <h1>Transaction Search</h1>
+                <TransactionList budget_id={budget_id} />
+            </div>
+            <div>  
+                <button className="rounded-full px-4 py-2 bg-blue-500 text-white" onClick={(e) => navigate('/profile')}>Cancel</button>
+            </div>
+        </>
     )
 
 }
